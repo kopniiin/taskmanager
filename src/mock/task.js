66db3f90624929ca07f5668711e633fba4533bbd
kopniiin <kopniiin@gmail.com>
2020-04-10 22:21:52 +0300

    Добавляет моки задач

---
+++ src/mock/task.js
@@ -0,0 +1,50 @@
+import {
+  TASK_DESCRIPTIONS,
+  TASK_MAX_DATE_OFFSET,
+  TASK_DEFAULT_REPEATING_DAYS,
+  TASK_COLORS
+} from "../const";
+
+import {
+  getRandomBoolean,
+  getRandomInteger,
+  getRandomElement
+} from "../utils";
+
+const generateDeadline = () => {
+  const deadline = new Date();
+
+  const deadlineOffset = getRandomInteger(TASK_MAX_DATE_OFFSET);
+  const offsetSign = getRandomBoolean() ? 1 : -1;
+
+  deadline.setDate(deadline.getDate() + deadlineOffset * offsetSign);
+
+  return deadline;
+};
+
+const generateRepeatingDays = () => {
+  const repeatingDays = Object.assign({}, TASK_DEFAULT_REPEATING_DAYS);
+
+  for (const day of Object.keys(repeatingDays)) {
+    repeatingDays[day] = getRandomBoolean();
+  }
+
+  return repeatingDays;
+};
+
+const generateTask = () => {
+  const dueDate = getRandomBoolean() ? generateDeadline() : null;
+
+  return {
+    description: getRandomElement(TASK_DESCRIPTIONS),
+    dueDate,
+    repeatingDays: dueDate ? TASK_DEFAULT_REPEATING_DAYS : generateRepeatingDays(),
+    color: getRandomElement(TASK_COLORS),
+    isFavorite: getRandomBoolean(),
+    isArchive: getRandomBoolean()
+  };
+};
+
+const generateTasks = (amount) => new Array(amount).fill(``).map(generateTask);
+
+export {generateTasks};
