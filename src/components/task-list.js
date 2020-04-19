6575077edf29e3564be5ad952ed1e56330e94bf5
kopniiin <kopniiin@gmail.com>
2020-04-19 23:36:26 +0300

    Добавляет список задач и сортировку

---
+++ src/components/task-list.js
@@ -0,0 +1,25 @@
+import {createElementFromTemplate} from "../utils";
+
+const createTaskListTemplate = () => `<div class="board__tasks"></div>`;
+
+export default class TaskList {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskListTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElementFromTemplate(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
