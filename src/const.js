68e70e7288ef287aa72f7c4814b477cfbda416e4
kopniiin <kopniiin@gmail.com>
2020-05-05 12:00:40 +0300

    Подключает moment

--- src/const.js
+++ src/const.js
@@ -1,20 +1,5 @@
 export const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
 
-export const MONTHS = [
-  `January`,
-  `February`,
-  `March`,
-  `April`,
-  `May`,
-  `June`,
-  `July`,
-  `August`,
-  `September`,
-  `October`,
-  `November`,
-  `December`,
-];
-
 export const RenderPosition = {
   AFTERBEGIN: `afterbegin`,
   BEFOREEND: `beforeend`
