76ece34d1b0d42414d574f2897cae6b464e628be
kopniiin <kopniiin@gmail.com>
2020-05-05 10:55:46 +0300

    Подключает flatpickr

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,13 @@ module.exports = {
     contentBase: path.join(__dirname, 'public'),
     watchContentBase: true,
     open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 };
